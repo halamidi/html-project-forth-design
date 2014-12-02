d9fc328987206291350d57df2f0ee3cbcaa6b138
halamidi <h.alamidi@yahoo.co.uk>
2014-12-02 18:46:56 +0000

    eight commit
    merged barrys code with mine and some miner tweeks

--- js/behaviour.js
+++ js/behaviour.js
@@ -336,6 +336,10 @@ $(document).ready(function(){
 				$("#sidediv").css("height", "863px");
 				$("#sidepg2").css("height", "1140px");
 			}
+			else if(visiblediv > 6){
+			$("#centersearch").css("overflow-y", "scroll");
+			
+			}
 			else{
 				$("#searchmain").css("height", "725px");
 				$("#centersearch").css("height", "713px");
@@ -345,3 +349,50 @@ $(document).ready(function(){
 			}
         });
     });
+	
+	
+		//DROPDOWN MENU
+	
+	
+var timeout	= 100;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{	
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
+}
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
+
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose; 
