5a969cd467855061d4db9359bca468c9c7e18c3d
halamidi <h.alamidi@yahoo.co.uk>
2014-12-07 20:22:05 +0000

    test
    not an update just redone code as trying something out

--- js/behaviour.js
+++ js/behaviour.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
 			}
         });
     });
-*/
+*//*
 $(document).ready(function(){
     $('.searchbutton, #sidebtn').click(window.test = function test(){
 		//reset display style
@@ -351,7 +351,7 @@ $(document).ready(function(){
 				$("#sidepg2").css("height", "752px");
 			}
         });
-    });
+    });*/
 	
 	
 		//DROPDOWN MENU
@@ -399,3 +399,294 @@ function mcancelclosetime()
 
 // close layer when click-out
 document.onclick = mclose; 
+
+$(document).ready(window.test = function test(){
+		//reset display style
+		$("#service2").css("display", "none");
+		$("#service1").css("display", "none");
+		$("#service3").css("display", "none");
+		$("#service4").css("display", "none");
+		$("#service5").css("display", "none");
+		$("#service6").css("display", "none");
+		//removes paragraph
+		$( "p" ).remove( ":contains('no results found')" );
+		//declare and initialize variable 
+		var check = document.getElementById("erase").value;
+		var toAdd;
+		if(check != "find services in your area"){
+			check = check.toUpperCase();
+			$.cookie('myCookie', $(check), { path: '/'});
+			toAdd = $.cookie('myCookie');
+		}
+		alert($.cookie("myCookie"));
+		alert(toAdd);
+        //var toAdd = $('#erase').val();
+		
+		var cat = $('#category').val();
+		var loc = $('#location').val();
+		//create array not sure if i intend on using array in this version
+		var hickeys2 = ["PHARMACY", "HICKEYS"];
+		var found = (hickeys2.indexOf(toAdd) > -1);
+		var dennis2 = ["DENNIS", "GP"];
+		var found2 = (dennis2.indexOf(toAdd) > -1);
+		//search parameters
+		//is it a pharmacy
+    if(cat === "Pharmacy"){
+		//is it in dublin
+			if(loc === "Dublin")
+				{
+				//keywords
+				 switch(toAdd){
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					default:
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+					}	
+				}
+				//is it in wicklow
+				else if(loc == "Wicklow")
+				{
+				 switch(toAdd){
+				 //keywords
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service4").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					case 'ROCHES':
+						$("#service4").css("display", "block");
+					break;
+					default:
+						$("#service1").css("display", "block");
+						$("#service4").css("display", "block");
+						$("#service5").css("display", "block");
+					}	
+				}
+				else{
+				switch(toAdd){
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service4").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					case 'ROCHES':
+						$("#service4").css("display", "block");
+					break;
+					default:
+						$("#service1").css("display", "block");
+						$("#service4").css("display", "block");
+						$("#service5").css("display", "block");
+					}	
+				}
+	}
+			//is it a gp
+	else if(cat == "GP"){
+			//is it in dublin
+				if(loc == "Dublin")
+				{
+				//keywords
+				 switch(toAdd){
+					case 'DENNIS':
+						$("#service2").css("display", "block");
+					break;
+					case 'MITCHELLS':
+						$("#service3").css("display", "block");
+					break;
+					case 'GP':
+					case 'DOCTORS':
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+					break;
+					default:
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+					}	
+				}
+				else if(loc == "Wicklow")
+				{
+				 switch(toAdd){
+					case 'DENNIS':
+						$("#service3").css("display", "block");
+					break;
+					case 'GP':
+					case 'DOCTORS':
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					break;
+					default:
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					}	
+				}
+				else{
+				switch(toAdd){
+					case 'DENNIS':
+						$("#service2").css("display", "block");
+					break;
+					case 'MITCHELLS':
+						$("#service3").css("display", "block");
+					break;
+					case 'GP':
+					case 'DOCTORS':
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					break;
+					default:
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					}	
+				}
+	}
+	else{
+				if(loc == "Dublin")
+				{
+				//keywords
+				 switch(toAdd){
+					case 'DENNIS':
+						$("#service2").css("display", "block");
+					break;
+					case 'MITCHELLS':
+						$("#service3").css("display", "block");
+					break;
+					case 'GP':
+					case 'DOCTORS':
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+					break;
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					default:
+						$("#service3").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service1").css("display", "block");
+						$("#service2").css("display", "block");
+					}	
+				}
+				//is it in wicklow
+				else if(loc == "Wicklow")
+				{
+				 switch(toAdd){
+				 //keywords
+					case 'GP':
+					case 'DOCTORS':
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					break;
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service4").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					case 'ROCHES':
+						$("#service4").css("display", "block");
+					break;
+					default:
+						$("#service1").css("display", "block");
+						$("#service4").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					}	
+				}
+				else{
+				switch(toAdd){
+					case 'DENNIS':
+						$("#service2").css("display", "block");
+					break;
+					case 'MITCHELLS':
+						$("#service3").css("display", "block");
+					break;
+					case 'DOCTORS':
+					case 'GP':
+						$("#service2").css("display", "block");
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+					break;
+					case 'PHARMACY':
+						$("#service1").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service4").css("display", "block");
+					break;
+					case 'HICKEYS':
+						$("#service1").css("display", "block");
+					break;
+					case 'BOOTS':
+						$("#service5").css("display", "block");
+					break;
+					case 'ROCHES':
+						$("#service4").css("display", "block");
+					break;
+					case 'ALL':
+					//just used as test
+						$("#service1").css("display", "block");
+						$("#service4").css("display", "block");
+						$("#service5").css("display", "block");
+						$("#service3").css("display", "block");
+						$("#service6").css("display", "block");
+						$("#service2").css("display", "block");
+					break;
+					default:
+						$('<p>no results found</p>').appendTo('#centersearch');
+					}	
+				}
+		}
+		//changes height of divs depending on how many child divs are visible
+		//for now there are only 6 child divs available so there is no need to check for more than one 
+		var visiblediv = $('#centersearch > :visible').length
+			if(visiblediv > 5){
+				$("#searchmain").css("height", "875px");
+				$("#centersearch").css("height", "863px");
+				$("#largeside2").css("height", "875px");
+				$("#sidediv").css("height", "863px");
+				$("#sidepg2").css("height", "900px");
+			}
+			else if(visiblediv > 6){
+			$("#centersearch").css("overflow-y", "scroll");
+			
+			}
+			else{
+				$("#searchmain").css("height", "725px");
+				$("#centersearch").css("height", "713px");
+				$("#largeside2").css("height", "725px");
+				$("#sidediv").css("height", "713px");
+				$("#sidepg2").css("height", "752px");
+			}
+ 
+    });
